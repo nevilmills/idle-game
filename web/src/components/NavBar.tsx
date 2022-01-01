6eeaa2a8bc3652928a5c55c7cde91d02ac3fbcfc
Nevil <nevilmills@gmail.com>
2021-12-31 16:32:58 -0800

    update navbar with logout functionality

--- web/src/components/NavBar.tsx
+++ web/src/components/NavBar.tsx
@@ -1,31 +1,67 @@
 import { Box, Button, Flex } from "@chakra-ui/react";
+import { useRouter } from "next/router";
 import React from "react";
-import { useMeQuery } from "../generated/graphql";
+import { useLogoutMutation, useMeQuery } from "../generated/graphql";
 
 interface NavBarProps {}
 
 export const NavBar: React.FC<NavBarProps> = ({}) => {
   const [{ data }] = useMeQuery();
+  const [, logout] = useLogoutMutation();
+  const router = useRouter();
 
-  return (
-    <Box
-      height={50}
-      bgColor="teal"
-      display="flex"
-      justifyContent="flex-end"
-      alignItems="center"
-    >
+  const handleClick = () => {
+    logout();
+    router.reload();
+  };
+
+  if (!data?.me) {
+    return (
       <Box
+        height={50}
+        bgColor="teal"
         display="flex"
-        justifyContent="space-evenly"
-        mr={8}
+        justifyContent="flex-end"
         alignItems="center"
       >
-        <Flex>{data?.me.username}</Flex>
-        <Button size="sm" ml={2} background="transparent">
-          logout
-        </Button>
+        <Box
+          display="flex"
+          justifyContent="space-evenly"
+          mr={8}
+          alignItems="center"
+        >
+          <Button size="sm" ml={2} background="transparent">
+            login
+          </Button>
+        </Box>
       </Box>
-    </Box>
-  );
+    );
+  } else {
+    return (
+      <Box
+        height={50}
+        bgColor="teal"
+        display="flex"
+        justifyContent="flex-end"
+        alignItems="center"
+      >
+        <Box
+          display="flex"
+          justifyContent="space-evenly"
+          mr={8}
+          alignItems="center"
+        >
+          <Flex>{data?.me.username}</Flex>
+          <Button
+            size="sm"
+            ml={2}
+            background="transparent"
+            onClick={handleClick}
+          >
+            logout
+          </Button>
+        </Box>
+      </Box>
+    );
+  }
 };
