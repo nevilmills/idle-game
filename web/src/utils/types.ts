08a339f1c89ea79bda52eee81910d2feb08f2795
Nevil <nevilmills@gmail.com>
2022-02-05 08:58:46 -0800

    update types

--- web/src/utils/types.ts
+++ web/src/utils/types.ts
@@ -1,6 +1,13 @@
 import { Dispatch, SetStateAction } from "react";
+import { UseQueryArgs } from "urql";
+import { Exact } from "../generated/graphql";
 
 export type ctxParam = {
   area: JSX.Element;
   setArea: Dispatch<SetStateAction<JSX.Element>>;
 } | null;
+
+export type queryArgs = Omit<
+  UseQueryArgs<Exact<{ name: string }>, any>,
+  "query"
+>;
