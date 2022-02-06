86492627bc35eceec60fc39d022e77788f76da73
Nevil <nevilmills@gmail.com>
2022-02-05 19:52:02 -0800

    add tree experience to constants

--- web/src/utils/constants.ts
+++ web/src/utils/constants.ts
@@ -4,3 +4,12 @@ export const GSA = "#A3F7B5";
 export const C_GREEN = "#40C9A2";
 export const V_GREEN = "#2F9C95";
 export const ROSE = "#664147";
+
+// woodcutting experience
+export const trees = {
+  normal: 25,
+  oak: 37.5,
+  willow: 67.5,
+  maple: 100,
+  yew: 175,
+};
