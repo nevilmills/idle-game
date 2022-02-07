62d5a6e2e0eeea88712b023a9c8bd20c376fb58b
Nevil <nevilmills@gmail.com>
2022-02-07 13:24:30 -0800

    clean up Character_Skill.ts

--- server/src/entities/Character_Skill.ts
+++ server/src/entities/Character_Skill.ts
@@ -1,14 +1,5 @@
 import { Field, ObjectType } from "type-graphql";
-import {
-  BaseEntity,
-  Column,
-  Entity,
-  JoinColumn,
-  ManyToOne,
-  OneToOne,
-  PrimaryColumn,
-  PrimaryGeneratedColumn,
-} from "typeorm";
+import { BaseEntity, Column, Entity, ManyToOne, PrimaryColumn } from "typeorm";
 import { Character } from "./Character";
 import { Skill } from "./Skill";
 
@@ -37,27 +28,3 @@ export class Character_Skill extends BaseEntity {
   @ManyToOne(() => Skill, (skill) => skill.character_skills)
   skill: Skill;
 }
-
-// @ObjectType()
-// @Entity()
-// export class Character_Skill extends BaseEntity {
-//   @Field()
-//   @PrimaryGeneratedColumn()
-//   id!: number;
-
-//   @Field()
-//   @Column({ default: 1 })
-//   level: number;
-
-//   @Field()
-//   @Column({ default: 0 })
-//   xp: number;
-
-//   @Field(() => Character)
-//   @ManyToOne(() => Character, (character) => character.skills)
-//   character: Character;
-
-//   @Field(() => Skill)
-//   @ManyToOne(() => Skill, (skill) => skill.character_skills)
-//   skill: Skill;
-// }
