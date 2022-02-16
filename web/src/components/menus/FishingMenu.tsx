4325880af1a25bf824b4e3cf161b60a68d3f350b
Nevil <nevilmills@gmail.com>
2022-02-15 20:32:00 -0800

    update fishing menu

--- web/src/components/menus/FishingMenu.tsx
+++ web/src/components/menus/FishingMenu.tsx
@@ -1,15 +1,24 @@
 import { Box } from "@chakra-ui/react";
 import React from "react";
+import { useGetSkillIdQuery } from "../../generated/graphql";
 import { V_GREEN } from "../../utils/constants";
 import { Trainer } from "../Trainer";
 
 interface FishingMenuProps {}
 
 export const FishingMenu: React.FC<FishingMenuProps> = ({}) => {
+  const [{ data }] = useGetSkillIdQuery({
+    variables: { name: "fishing" },
+  });
+
+  if (!data?.getSkillId.id) {
+    return <Box>Error fetching data</Box>;
+  }
+
   return (
     <Box w="100%" h="100vh" backgroundColor={V_GREEN}>
       fishing area
-      <Trainer />
+      <Trainer skillId={data?.getSkillId.id} />
     </Box>
   );
 };
