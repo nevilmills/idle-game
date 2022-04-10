29d1376c0950a5e78a63e70a41f1409c79cdf894
Nevil <nevilmills@gmail.com>
2022-04-09 18:00:52 -0700

    add fish + ore to constants file

--- web/src/utils/constants.ts
+++ web/src/utils/constants.ts
@@ -45,3 +45,81 @@ export const trees = [
     id: 5,
   },
 ];
+
+// fishing experience
+export const fish = [
+  {
+    name: "Shrimp",
+    exp: 25,
+    time: 1750,
+    levelReq: 1,
+    id: 1,
+  },
+  {
+    name: "Salmon",
+    exp: 37.5,
+    time: 2250,
+    levelReq: 15,
+    id: 2,
+  },
+  {
+    name: "Swordfish",
+    exp: 67.5,
+    time: 2750,
+    levelReq: 30,
+    id: 3,
+  },
+  {
+    name: "Lobster",
+    exp: 100,
+    time: 3000,
+    levelReq: 45,
+    id: 4,
+  },
+  {
+    name: "Shark",
+    exp: 175,
+    time: 4500,
+    levelReq: 60,
+    id: 5,
+  },
+];
+
+// mining experience
+export const ore = [
+  {
+    name: "Copper",
+    exp: 25,
+    time: 1750,
+    levelReq: 1,
+    id: 1,
+  },
+  {
+    name: "Iron",
+    exp: 37.5,
+    time: 2250,
+    levelReq: 15,
+    id: 2,
+  },
+  {
+    name: "Mithril",
+    exp: 67.5,
+    time: 2750,
+    levelReq: 30,
+    id: 3,
+  },
+  {
+    name: "Adamantite",
+    exp: 100,
+    time: 3000,
+    levelReq: 45,
+    id: 4,
+  },
+  {
+    name: "Runite",
+    exp: 175,
+    time: 4500,
+    levelReq: 60,
+    id: 5,
+  },
+];
